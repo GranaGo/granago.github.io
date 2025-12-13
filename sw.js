ab53eff569ab22e63d23cb038090c74cfc30e919
GranaGo <GranaGo@users.noreply.github.com>
2025-12-13 11:56:33 +0000

    deploy: 2df5447a81c1aa0bc39c1952a2f5ad8f15264c35

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    GRANÁGO - SERVICE WORKER (SIN WORKBOX) - API + CACHE BUSTING
    =============================================================== */
 
-const CACHE_VERSION = 'v5.0'; // Tu script update-cache-version lo actualizará
+const CACHE_VERSION = 'v5.1'; // Tu script update-cache-version lo actualizará
 const STATIC_CACHE = `granago-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `granago-dynamic-${CACHE_VERSION}`;
 const API_CACHE = `granago-api-${CACHE_VERSION}`;
